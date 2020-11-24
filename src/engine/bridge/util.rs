d1238f9bf3fc4f5c8e7dfae6ad622973f50c5ce1
Vladimir Petrzhikovskiy <v.petrzhikovskiy@dexpa.io>
2020-11-24 19:21:02 +0300

    WIP: check it

--- src/engine/bridge/util.rs
+++ src/engine/bridge/util.rs
@@ -1,9 +1,12 @@
 use anyhow::anyhow;
 use anyhow::Error;
 use ethabi::ParamType;
+use ethabi::Token as EthTokenValue;
+use num_bigint::{BigInt, BigUint};
 use serde::{Deserialize, Serialize};
 use sha3::digest::Digest;
 use sha3::Keccak256;
+use ton_abi::TokenValue as TonTokenValue;
 
 use relay_eth::ws::H256;
 
@@ -32,6 +35,36 @@ pub fn from_str(token: &str) -> Result<ParamType, Error> {
         }
         _ => unimplemented!(),
     })
+}
+
+fn map_eth_ton(eth: EthTokenValue) -> TonTokenValue {
+    match eth {
+        EthTokenValue::FixedBytes(x) => TonTokenValue::FixedBytes(x.to_vec()),
+        EthTokenValue::Bytes(x) => TonTokenValue::Bytes(x.to_vec()),
+        EthTokenValue::Uint(x) => {
+            let mut bytes = [0u8; 256 / 8];
+            x.to_big_endian(&mut bytes);
+            let number = BigUint::from_bytes_be(&bytes);
+            TonTokenValue::Uint(ton_abi::Uint { number, size: 256 }) //fixme ? check correctness
+        }
+        EthTokenValue::Int(x) => {
+            let mut bytes = [0u8; 256 / 8];
+            x.to_big_endian(&mut bytes);
+
+            let sign = bytes[0] & 0x80;
+
+            let sign = if sign == 1 {
+                num_bigint::Sign::Plus
+            } else {
+                num_bigint::Sign::Minus
+            };
+            dbg!(&sign);
+            let number = BigInt::from_signed_bytes_be(&bytes);
+            TonTokenValue::Int(ton_abi::Int { number, size: 256 }) //fixme ? check correctness
+        }
+        // TonTokenValue::Uint(x.as_byte_slice()) }
+        _ => unimplemented!(),
+    }
 }
 
 pub fn abi_to_topic_hash(abi: &str) -> Result<H256, Error> {
@@ -85,13 +118,14 @@ pub fn abi_to_topic_hash(abi: &str) -> Result<H256, Error> {
 #[cfg(test)]
 mod test {
     use ethabi::ParamType;
-
+    use ethabi::Token as EthTokenValue;
     use sha3::Digest;
     use sha3::Keccak256;
+    use ton_abi::TokenValue as TonTokenValue;
 
     use relay_eth::ws::H256;
 
-    use crate::engine::bridge::util::{abi_to_topic_hash, from_str};
+    use crate::engine::bridge::util::{abi_to_topic_hash, from_str, map_eth_ton};
 
     const ABI: &str = r#"
   {
@@ -145,5 +179,35 @@ mod test {
         let expected = ParamType::Address;
         let got = from_str("address").unwrap();
         assert_eq!(expected, got);
+    }
+
+    #[test]
+    fn test_conversion_uint() {
+        use ethabi::Uint as EUint;
+        use ton_abi::Uint as TUint;
+        let eth = EthTokenValue::Uint(EUint::from(1234567));
+        let ton_expected = TonTokenValue::Uint(TUint::new(1234567, 256));
+        assert_eq!(map_eth_ton(eth), ton_expected);
+    }
+
+    #[test]
+    fn test_conversion_int() {
+        use ethabi::Int as EInt;
+        use ton_abi::Int as TInt;
+        let eth_value: u64 = -1234567i64 as u64;
+
+
+        let eth = EthTokenValue::Int(EInt::from(eth_value));
+        let ton_expected = TonTokenValue::Int(TInt::new(-1234567, 256));
+        assert_eq!(map_eth_ton(eth), ton_expected);
+    }
+
+    #[test]
+    fn test_conversion_int_plus() {
+        use ethabi::Int as EInt;
+        use ton_abi::Int as TInt;
+        let eth = EthTokenValue::Int(EInt::from(1234567));
+        let ton_expected = TonTokenValue::Int(TInt::new(1234567, 256));
+        assert_eq!(map_eth_ton(eth), ton_expected);
     }
 }
