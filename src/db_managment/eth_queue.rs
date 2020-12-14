5c8cfa2a5869d9b538e63d3b1361925715a54352
Vladimir <v.petrzhikovskiy@dexpa.io>
2020-12-14 13:58:39 +0300

    Add locking for eth_queue

--- src/db_managment/eth_queue.rs
+++ src/db_managment/eth_queue.rs
@@ -1,36 +1,48 @@
 use std::collections::HashSet;
 
+use tokio::sync::Mutex;
+
+use relay_ton::prelude::HashMap;
+
 use crate::db_managment::models::EthTonConfirmationData;
 use crate::db_managment::Table;
 
 use super::prelude::*;
-use relay_ton::prelude::HashMap;
 
 #[derive(Clone)]
 pub struct EthQueue {
     db: Tree,
+    table_lock: Arc<Mutex<()>>,
 }
 
 impl EthQueue {
     pub fn new(db: &Db) -> Result<Self, Error> {
         Ok(Self {
             db: db.open_tree(super::constants::ETH_QUEUE_TREE_NAME)?,
+            table_lock: Arc::new(Mutex::new(())),
         })
     }
 
-    pub fn get(&self, key: u64) -> Result<Option<HashSet<EthTonConfirmationData>>, Error> {
+    pub async fn get(&self, key: u64) -> Result<Option<HashSet<EthTonConfirmationData>>, Error> {
+        let guard = self.table_lock.lock().await;
         match self.db.get(key.to_be_bytes())? {
             Some(data) => Ok(Some(bincode::deserialize(&data)?)),
             None => Ok(None),
         }
     }
 
-    pub fn remove(&self, key: u64) -> Result<(), Error> {
+    pub async fn remove(&self, key: u64) -> Result<(), Error> {
+        let guard = self.table_lock.lock().await;
         self.db.remove(key.to_be_bytes())?;
         Ok(())
     }
 
-    pub fn insert(&self, key: &u64, value: &HashSet<EthTonConfirmationData>) -> Result<(), Error> {
+    pub async fn insert(
+        &self,
+        key: &u64,
+        value: &HashSet<EthTonConfirmationData>,
+    ) -> Result<(), Error> {
+        let guard = self.table_lock.lock().await;
         self.db
             .insert(key.to_be_bytes(), bincode::serialize(&value).unwrap())?;
         Ok(())
