4d6806318b065abddce745d0816ca202f2e42a92
Vladimir <v.petrzhikovskiy@dexpa.io>
2021-09-08 14:43:13 +0300

    Add staking contract binding

--- src/engine/ton_contracts/staking_contract.rs
+++ src/engine/ton_contracts/staking_contract.rs
@@ -1,70 +1,87 @@
-use nekoton_abi::*;
+use nekoton_abi::FunctionBuilder;
 use once_cell::sync::OnceCell;
+use ton_abi::{Function, Param, ParamType};
 
 use super::models::*;
 
 /// External responsible function
-pub fn is_active() -> &'static ton_abi::Function {
-    static FUNCTION: OnceCell<ton_abi::Function> = OnceCell::new();
+pub fn is_active() -> &'static Function {
+    static FUNCTION: OnceCell<Function> = OnceCell::new();
     FUNCTION.get_or_init(|| {
         FunctionBuilder::new_responsible("isActive")
             .time_header()
-            .output("is_active", ton_abi::ParamType::Bool)
+            .output("is_active", ParamType::Bool)
             .build()
     })
 }
 
 /// External function
-pub fn current_relay_round() -> &'static ton_abi::Function {
-    static FUNCTION: OnceCell<ton_abi::Function> = OnceCell::new();
+pub fn current_relay_round() -> &'static Function {
+    static FUNCTION: OnceCell<Function> = OnceCell::new();
     FUNCTION.get_or_init(|| {
         FunctionBuilder::new("currentRelayRound")
             .time_header()
-            .output("round", ton_abi::ParamType::Uint(32))
+            .output("round", ParamType::Uint(32))
             .build()
     })
 }
 
 /// External responsible function
-pub fn get_relay_round_address() -> &'static ton_abi::Function {
-    static FUNCTION: OnceCell<ton_abi::Function> = OnceCell::new();
+pub fn get_relay_round_address() -> &'static Function {
+    static FUNCTION: OnceCell<Function> = OnceCell::new();
     FUNCTION.get_or_init(|| {
         FunctionBuilder::new_responsible("getRelayRoundAddress")
             .time_header()
-            .input("round_num", ton_abi::ParamType::Uint(32))
-            .output("address", ton_abi::ParamType::Address)
+            .input("round_num", ParamType::Uint(32))
+            .output("address", ParamType::Address)
             .build()
     })
 }
 
 /// External responsible function
-pub fn get_relay_round_address_from_timestamp() -> &'static ton_abi::Function {
-    static FUNCTION: OnceCell<ton_abi::Function> = OnceCell::new();
+pub fn get_relay_round_address_from_timestamp() -> &'static Function {
+    static FUNCTION: OnceCell<Function> = OnceCell::new();
     FUNCTION.get_or_init(|| {
         FunctionBuilder::new_responsible("getRelayRoundAddressFromTimestamp")
             .time_header()
-            .input("time", ton_abi::ParamType::Uint(32))
-            .output("address", ton_abi::ParamType::Address)
+            .input("time", ParamType::Uint(32))
+            .output("address", ParamType::Address)
             .build()
     })
 }
 
 /// External function
-pub fn current_relay_round_start_time() -> &'static ton_abi::Function {
-    static FUNCTION: OnceCell<ton_abi::Function> = OnceCell::new();
+pub fn current_relay_round_start_time() -> &'static Function {
+    static FUNCTION: OnceCell<Function> = OnceCell::new();
     FUNCTION.get_or_init(|| {
         FunctionBuilder::new("currentRelayRoundStartTime")
             .time_header()
-            .output("start_time", ton_abi::ParamType::Uint(32))
+            .output("start_time", ParamType::Uint(32))
+            .build()
+    })
+}
+
+/// External function
+pub fn confirm_eth_account() -> &'static Function {
+    static FUNCTION: OnceCell<Function> = OnceCell::new();
+    FUNCTION.get_or_init(|| {
+        FunctionBuilder::new("confirmEthAccount")
+            .input("staker_addr", ParamType::Address)
+            .input("eth_address", ParamType::Uint(160))
+            .input("send_gas_to", ParamType::Address)
+            .time_header()
             .build()
     })
 }
 
 pub mod events {
+    use nekoton_abi::{EventBuilder, KnownParamTypePlain};
+    use ton_abi::Event;
+
     use super::*;
 
-    pub fn relay_round_initialized() -> &'static ton_abi::Event {
-        static EVENT: OnceCell<ton_abi::Event> = OnceCell::new();
+    pub fn relay_round_initialized() -> &'static Event {
+        static EVENT: OnceCell<Event> = OnceCell::new();
         EVENT.get_or_init(|| {
             EventBuilder::new("RelayRoundInitialized")
                 .inputs(RelayRoundInitializedEvent::param_type())
@@ -72,11 +89,11 @@ pub mod events {
         })
     }
 
-    pub fn bridge_updated() -> &'static ton_abi::Event {
-        static EVENT: OnceCell<ton_abi::Event> = OnceCell::new();
+    pub fn bridge_updated() -> &'static Event {
+        static EVENT: OnceCell<Event> = OnceCell::new();
         EVENT.get_or_init(|| {
             EventBuilder::new("BridgeUpdated")
-                .input("new_bridge", ton_abi::ParamType::Address)
+                .input("new_bridge", ParamType::Address)
                 .build()
         })
     }
