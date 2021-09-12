f8ccb9b34e0c0c7d77115cbddd5ffe40fe8a4cb8
Ivan Kalinin <i.kalinin@dexpa.io>
2021-09-12 14:41:59 +0300

    Fix outputs

--- src/engine/ton_contracts/elections_contract.rs
+++ src/engine/ton_contracts/elections_contract.rs
@@ -1,16 +1,15 @@
 use nekoton_abi::*;
 use once_cell::sync::OnceCell;
 
+use super::models::*;
+
 pub fn staker_addrs() -> &'static ton_abi::Function {
     static FUNCTION: OnceCell<ton_abi::Function> = OnceCell::new();
     FUNCTION.get_or_init(|| {
         FunctionBuilder::new("staker_addrs")
             .time_header()
             .expire_header()
-            .output(
-                "staker_addrs",
-                ton_abi::ParamType::Array(Box::new(ton_abi::ParamType::Address)),
-            )
+            .outputs(StakerAddresses::param_type())
             .build()
     })
 }
