3e609fcb3ed43fd9cffffd655fe1c192b3773564
Ivan Kalinin <i.kalinin@dexpa.io>
2021-10-04 16:21:40 +0300

    Update dependencies

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -152,6 +152,8 @@ impl NodeConfig {
             adnl_keys: temp_keys.into(),
             rocks_db_path: self.db_path.join("rocksdb"),
             file_db_path: self.db_path.join("files"),
+            // NOTE: State GC is disabled until it is fully tested
+            state_gc_options: None,
             old_blocks_policy: Default::default(),
             shard_state_cache_enabled: false,
             max_db_memory_usage: self.max_db_memory_usage,
