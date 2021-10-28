c0779fd5ad782bd81a1b9c113cf5e2b29b51d74c
Ivan Kalinin <i.kalinin@dexpa.io>
2021-10-28 13:16:59 +0300

    Fix paths

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 set -eE
 
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)
-REPO_DIR="$SCRIPT_DIR/../"
+REPO_DIR="$SCRIPT_DIR/.."
 
 function print_help() {
   echo 'Usage: setup.sh [OPTIONS]'
