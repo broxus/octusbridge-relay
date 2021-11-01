ee573d4db7e611c40ca047bdf0c4fb3aeba40eb7
Ivan Kalinin <i.kalinin@dexpa.io>
2021-11-01 17:29:19 +0300

    Reload logger settings on SIGHUP

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use std::borrow::Cow;
 use anyhow::{Context, Result};
 use argh::FromArgs;
 use dialoguer::{Confirm, Input, Password};
+use futures::future::Future;
 use relay::config::*;
 use relay::engine::*;
 use serde::{Deserialize, Serialize};
+use tokio::signal::unix;
 use tokio::sync::mpsc;
 
 #[tokio::main]
@@ -51,11 +53,16 @@ struct CmdRun {
 
 impl CmdRun {
     async fn execute(self) -> Result<()> {
-        let config: AppConfig = read_config(self.config)?;
+        let config: AppConfig = read_config(&self.config)?;
         let global_config = ton_indexer::GlobalConfig::from_file(&self.global_config)
             .context("Failed to open global config")?;
 
-        init_logger(&config.logger_settings).context("Failed to init logger")?;
+        init_logger(&config.logger_settings, move || {
+            futures::future::ready(
+                read_config(self.config.clone()).map(|config: AppConfig| config.logger_settings),
+            )
+        })
+        .context("Failed to init logger")?;
 
         log::info!("Initializing relay...");
 
@@ -251,8 +258,48 @@ where
     config.try_into().context("Failed to parse config")
 }
 
-fn init_logger(config: &serde_yaml::Value) -> Result<()> {
-    let config = serde_yaml::from_value(config.clone())?;
-    log4rs::config::init_raw_config(config)?;
+fn init_logger<F, R>(initial_value: &serde_yaml::Value, mut on_reload: F) -> Result<()>
+where
+    F: FnMut() -> R + Send + Sync + 'static,
+    R: Future<Output = Result<serde_yaml::Value>> + Send + Sync + 'static,
+{
+    fn parse_config(value: serde_yaml::Value) -> Result<log4rs::Config> {
+        let config = serde_yaml::from_value::<log4rs::config::RawConfig>(value)?;
+
+        let (appenders, errors) = config.appenders_lossy(&log4rs::config::Deserializers::default());
+        if !errors.is_empty() {
+            return Err(InitError::Deserializing).with_context(|| format!("{:#?}", errors));
+        }
+
+        let config = log4rs::Config::builder()
+            .appenders(appenders)
+            .loggers(config.loggers())
+            .build(config.root())?;
+        Ok(config)
+    }
+
+    let handle = log4rs::config::init_config(parse_config(initial_value.clone())?)?;
+    let mut signals_stream =
+        unix::signal(unix::SignalKind::hangup()).context("Failed to subscribe to SIGHUP signal")?;
+
+    tokio::spawn(async move {
+        while let Some(()) = signals_stream.recv().await {
+            match on_reload().await.and_then(parse_config) {
+                Ok(config) => {
+                    handle.set_config(config);
+                }
+                Err(e) => {
+                    log::error!("Failed to reload config: {:?}", e);
+                }
+            };
+        }
+    });
+
     Ok(())
+}
+
+#[derive(thiserror::Error, Debug)]
+enum InitError {
+    #[error("Errors found when deserializing the logger config")]
+    Deserializing,
 }
