d06aad11b75715a35c31d1bf5b0e379bf8ea07c2
Alexey Pashinov <pashinov93@gmail.com>
2022-03-31 19:59:55 +0200

    WIP: solana subscriber

--- src/config/sol_config.rs
+++ src/config/sol_config.rs
@@ -1,14 +1,32 @@
 use serde::{Deserialize, Serialize};
 use solana_sdk::commitment_config::CommitmentConfig;
-use url::Url;
 
 /// Solana network settings
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct SolConfig {
     /// RPC endpoint
-    pub endpoint: Url,
+    pub url: String,
+
+    /// RPC WS endpoint
+    pub ws_url: String,
 
     /// Commitment level
-    pub commitment: CommitmentConfig,
+    pub commitment_config: CommitmentConfig,
+
+    /// Timeout, used for simple getter requests. Default: 10
+    #[serde(default = "default_get_timeout_sec")]
+    pub get_timeout_sec: u64,
+
+    /// Max request duration (including all failed retires). Default: 604800
+    #[serde(default = "default_maximum_failed_responses_time_sec")]
+    pub maximum_failed_responses_time_sec: u64,
+}
+
+fn default_get_timeout_sec() -> u64 {
+    10
+}
+
+fn default_maximum_failed_responses_time_sec() -> u64 {
+    604800
 }
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -4,6 +4,7 @@ pub use self::pending_messages_queue::*;
 pub use self::retry::*;
 pub use self::serde_helpers::*;
 pub use self::shard_utils::*;
+pub use self::solana_helper::*;
 pub use self::tristate::*;
 pub use self::tx_context::*;
 
@@ -13,6 +14,7 @@ mod pending_messages_queue;
 mod retry;
 mod serde_helpers;
 mod shard_utils;
+mod solana_helper;
 mod tristate;
 mod tx_context;
 
