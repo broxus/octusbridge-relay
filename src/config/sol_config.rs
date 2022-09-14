859918fe96a1a42afe028533ee900b862568970c
Alexey Pashinov <pashinov93@gmail.com>
2022-09-14 12:31:49 +0200

    Update default intervals in solana settings

--- src/config/sol_config.rs
+++ src/config/sol_config.rs
@@ -22,16 +22,16 @@ pub struct SolConfig {
     #[serde(default = "const_usize::<10>")]
     pub pool_size: usize,
 
-    /// Blocks polling interval. Default: 60
-    #[serde(default = "const_u64::<60>")]
+    /// Blocks polling interval. Default: 15
+    #[serde(default = "const_u64::<15>")]
     pub poll_interval_sec: u64,
 
     /// Max request duration (including all failed retires). Default: 604800
     #[serde(default = "const_u64::<604800>")]
     pub maximum_failed_responses_time_sec: u64,
 
-    /// Proposals polling interval. Default: 900
-    #[serde(default = "const_u64::<900>")]
+    /// Proposals polling interval. Default: 3600
+    #[serde(default = "const_u64::<3600>")]
     pub poll_proposals_interval_sec: u64,
 
     /// Skip invalid TON->SOL event is Solana. Default: false
