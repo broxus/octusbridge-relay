61ec42d91ea9159411a2595a7bf89f7e5f3ee9be
Alexey Pashinov <pashinov93@gmail.com>
2023-03-20 22:58:18 +0100

    Store utxo in db

--- src/engine/btc_subscriber/db/migrations/v1_0_0.rs
+++ src/engine/btc_subscriber/db/migrations/v1_0_0.rs
@@ -9,15 +9,15 @@ use super::Migrations;
 // 1.0.0
 pub(super) fn register(migrations: &mut Migrations) -> Result<()> {
     migrations.register([0, 0, 0], [1, 0, 0], |db| async move {
-        new_utxos_columns(&db)?; // TODO: check correctness of migration
+        new_utxo_column(&db)?;
         Ok(())
     })
 }
 
-fn new_utxos_columns(db: &Arc<rocksdb::DB>) -> Result<()> {
+fn new_utxo_column(db: &Arc<rocksdb::DB>) -> Result<()> {
     if Tree::<columns::UtxoBalances>::new(db).is_err() {
         let mut options = Default::default();
-        let caches = DbCaches::with_capacity(256)?;
+        let caches = DbCaches::with_capacity(DbCaches::MIN_CAPACITY)?;
         columns::UtxoBalances::options(&mut options, &caches);
         db.create_cf(columns::UtxoBalances::NAME, &options)?;
     }
