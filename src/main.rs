828a5554aac6c6856e96dbe64ca4c6a5a0b8bca7
Ivan Kalinin <i.kalinin@dexpa.io>
2023-05-15 11:51:31 +0200

    Add version flag

--- src/main.rs
+++ src/main.rs
@@ -21,7 +21,7 @@ fn main() -> Result<()> {
         tracing_subscriber::fmt::fmt().without_time().init();
     }
 
-    let app = argh::from_env::<App>();
+    let ArgsOrVersion::<App>(app) = argh::from_env();
     match app.command {
         Subcommand::Run(run) => run.execute(),
         Subcommand::Generate(generate) => generate.execute(),
@@ -356,3 +356,39 @@ where
 fn make_empty_password<'a>() -> Cow<'a, secstr::SecUtf8> {
     Cow::Owned("".into())
 }
+
+struct ArgsOrVersion<T: argh::FromArgs>(T);
+
+impl<T: argh::FromArgs> argh::TopLevelCommand for ArgsOrVersion<T> {}
+
+impl<T: argh::FromArgs> argh::FromArgs for ArgsOrVersion<T> {
+    fn from_args(command_name: &[&str], args: &[&str]) -> Result<Self, argh::EarlyExit> {
+        /// Also use argh for catching `--version`-only invocations
+        #[derive(argh::FromArgs)]
+        struct Version {
+            /// print version information and exit
+            #[argh(switch, short = 'v')]
+            pub version: bool,
+        }
+
+        match Version::from_args(command_name, args) {
+            Ok(v) if v.version => Err(argh::EarlyExit {
+                output: format!("{} {VERSION}", command_name.first().unwrap_or(&""),),
+                status: Ok(()),
+            }),
+            Err(exit) if exit.status.is_ok() => {
+                let help = match T::from_args(command_name, &["--help"]) {
+                    Ok(_) => unreachable!(),
+                    Err(exit) => exit.output,
+                };
+                Err(argh::EarlyExit {
+                    output: format!("{help}  -v, --version     print version information and exit"),
+                    status: Ok(()),
+                })
+            }
+            _ => T::from_args(command_name, args).map(Self),
+        }
+    }
+}
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
